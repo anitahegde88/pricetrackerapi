6044679e38d039a8eb6c0f28be4a661b0d225b52
Anita Hegde <anita.hegde@nhsbsa.nhs.uk>
2025-04-19 23:45:16 +0100

    added test to PriceTrackerServiceTest -add_newuser_to_user_table()

--- src/main/java/org/example/pricetracker_assignement/service/PriceTrackerService.java
+++ src/main/java/org/example/pricetracker_assignement/service/PriceTrackerService.java
@@ -3,12 +3,39 @@ package org.example.pricetracker_assignement.service;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import org.example.pricetracker_assignement.dto.PriceTrackerDTO;
+import org.example.pricetracker_assignement.entities.Users;
+import org.example.pricetracker_assignement.model.UsersTableUtils;
+import org.example.pricetracker_assignement.repository.UsersRepository;
+import org.example.pricetracker_assignement.utilities.FrequencyParser;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PriceTrackerService {
 
+    private final UsersRepository usersRepository;
+    private final UsersTableUtils usersTableUtils;
+    private final FrequencyParser frequencyParser;
+    public PriceTrackerService(FrequencyParser frequencyParser,
+                               UsersRepository usersRepository,
+                               UsersTableUtils usersTableUtils) {
+        this.usersRepository = usersRepository;
+        this.usersTableUtils = usersTableUtils;
+        this.frequencyParser = frequencyParser;
+    }
+
     public void trackPriceScheduler(@Valid PriceTrackerDTO priceTrackerDTO,
                                     @NotBlank String userName) {
+
+        long interval = frequencyParser.parseFrequency(priceTrackerDTO.getFrequency());
+        Optional<Users> user = usersRepository.findById(userName);
+       if(!user.isPresent()){
+           usersTableUtils.saveNewUserDetailsToUsersTable(userName,
+                   priceTrackerDTO.getProductUrl(),
+                   priceTrackerDTO.getDesiredPrice(),
+                   interval);
+       }
+
     }
 }
