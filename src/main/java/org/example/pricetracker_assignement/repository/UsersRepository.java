30885c8cfdbe61ab32c0ad82a8289fb50626c1d6
Anita Hegde <anita.hegde@nhsbsa.nhs.uk>
2025-04-20 00:54:04 +0100

    file reader utility and junit test and a json file to store actual price +added to sql to get users list who are eligible to get notification

--- src/main/java/org/example/pricetracker_assignement/repository/UsersRepository.java
+++ src/main/java/org/example/pricetracker_assignement/repository/UsersRepository.java
@@ -2,9 +2,18 @@ package org.example.pricetracker_assignement.repository;
 
 import org.example.pricetracker_assignement.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, String> {
 
+  @Query(
+      value =
+          "SELECT * FROM Users a "
+              + "WHERE DATEADD(MILLISECOND, a.FREQUENCY, a.LAST_RUN_TIME) <= CURRENT_TIMESTAMP",
+      nativeQuery = true)
+  List<Users> findUsersSchedulingTimeElapsed();
 }
