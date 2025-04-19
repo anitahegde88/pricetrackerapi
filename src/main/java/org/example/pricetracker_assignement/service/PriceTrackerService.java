d3eae824ee013ab172d4272545c45dc9a05008d3
Anita Hegde <anita.hegde@nhsbsa.nhs.uk>
2025-04-20 00:09:27 +0100

    added test and code to update the table if incoming user details already exist in the table

--- src/main/java/org/example/pricetracker_assignement/service/PriceTrackerService.java
+++ src/main/java/org/example/pricetracker_assignement/service/PriceTrackerService.java
@@ -4,38 +4,46 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import org.example.pricetracker_assignement.dto.PriceTrackerDTO;
 import org.example.pricetracker_assignement.entities.Users;
-import org.example.pricetracker_assignement.model.UsersTableUtils;
 import org.example.pricetracker_assignement.repository.UsersRepository;
 import org.example.pricetracker_assignement.utilities.FrequencyParser;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalTime;
 import java.util.Optional;
 
 @Service
 public class PriceTrackerService {
 
     private final UsersRepository usersRepository;
-    private final UsersTableUtils usersTableUtils;
     private final FrequencyParser frequencyParser;
     public PriceTrackerService(FrequencyParser frequencyParser,
-                               UsersRepository usersRepository,
-                               UsersTableUtils usersTableUtils) {
+                               UsersRepository usersRepository) {
         this.usersRepository = usersRepository;
-        this.usersTableUtils = usersTableUtils;
         this.frequencyParser = frequencyParser;
     }
 
+    @Transactional
     public void trackPriceScheduler(@Valid PriceTrackerDTO priceTrackerDTO,
                                     @NotBlank String userName) {
 
         long interval = frequencyParser.parseFrequency(priceTrackerDTO.getFrequency());
         Optional<Users> user = usersRepository.findById(userName);
-       if(!user.isPresent()){
-           usersTableUtils.saveNewUserDetailsToUsersTable(userName,
-                   priceTrackerDTO.getProductUrl(),
-                   priceTrackerDTO.getDesiredPrice(),
-                   interval);
+        if(user.isEmpty()) {
+            Users usersEntity = new Users();
+            usersEntity.setDesiredPrice(priceTrackerDTO.getDesiredPrice());
+            usersEntity.setFrequency(interval);
+            usersEntity.setProductUrl(priceTrackerDTO.getProductUrl());
+            usersEntity.setUserName(userName);
+            usersEntity.setLastRunTime(LocalTime.now());
+            usersRepository.saveAndFlush(usersEntity);
        }
 
+        if(user.isPresent()){
+            user.get().setDesiredPrice(priceTrackerDTO.getDesiredPrice());
+            user.get().setFrequency(interval);
+            user.get().setProductUrl(priceTrackerDTO.getProductUrl());
+            usersRepository.saveAndFlush(user.get());
+        }
     }
 }
--- src/test/java/org/example/pricetracker_assignement/service/PriceTrackerServiceTest.java
+++ src/test/java/org/example/pricetracker_assignement/service/PriceTrackerServiceTest.java
@@ -1,21 +1,29 @@
 package org.example.pricetracker_assignement.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 import org.example.pricetracker_assignement.dto.PriceTrackerDTO;
 import org.example.pricetracker_assignement.entities.Users;
 import org.example.pricetracker_assignement.repository.UsersRepository;
+import org.example.pricetracker_assignement.utilities.FrequencyParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 class PriceTrackerServiceTest {
 
     @Mock
     private UsersRepository usersRepository;
+
+    @Mock
+    private  FrequencyParser frequencyParser;
 
     @InjectMocks
     private PriceTrackerService priceTrackerService;
@@ -42,12 +50,31 @@ class PriceTrackerServiceTest {
             "Then all user sent info over the request must be saved onto database")
     void add_newuser_to_user_table(){
         PriceTrackerDTO priceTrackerDto = getPriceTrackerDTO();
-        Users users = new Users();
-        users.setUserName(USER_NAME);
-        users.setProductUrl(priceTrackerDto.getProductUrl());
-        users.setDesiredPrice(priceTrackerDto.getDesiredPrice());
-        users.setFrequency((60000L));
+        when(usersRepository.findById(any())).thenReturn(Optional.empty());
+        when(frequencyParser.parseFrequency(any())).thenReturn(60000L);
         priceTrackerService.trackPriceScheduler(priceTrackerDto, USER_NAME);
-        verify(usersRepository, times(1)).save(users);
+        ArgumentCaptor<Users> captor = ArgumentCaptor.forClass(Users.class);
+        verify(usersRepository, times(1)).saveAndFlush(captor.capture());
+        Users capturedUser = captor.getValue();
+        assertEquals(USER_NAME, capturedUser.getUserName());
+        assertEquals(priceTrackerDto.getProductUrl(), capturedUser.getProductUrl());
+        assertEquals(priceTrackerDto.getDesiredPrice(), capturedUser.getDesiredPrice());
+        assertEquals(60000L, capturedUser.getFrequency());
+    }
+
+    @Test
+    @DisplayName(value="Given a new user sends the request to hit the end point /product-price-tracker/scheduler" +
+            "with valid values" +
+            "And user already exists in the DB" +
+            "When trackPriceScheduler() method is called" +
+            "Then all user sent info over the request must be updated onto database")
+    void add_existing_user_details_to_table(){
+        PriceTrackerDTO priceTrackerDto = getPriceTrackerDTO();
+        when(usersRepository.findById(any())).thenReturn(Optional.of(new Users()));
+        when(frequencyParser.parseFrequency(any())).thenReturn(60000L);
+        priceTrackerService.trackPriceScheduler(priceTrackerDto, USER_NAME);
+        ArgumentCaptor<Users> captor = ArgumentCaptor.forClass(Users.class);
+        verify(usersRepository, times(1)).saveAndFlush(captor.capture());
     }
     }
+
